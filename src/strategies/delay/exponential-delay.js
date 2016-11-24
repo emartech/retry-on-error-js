04337a6a5faf8553008ea40c764db8dfdbdd0503
name <email>
2016-11-24 10:50:01 +0100

    fix(_multiplier): renamed private variable to use underscore

--- src/strategies/delay/exponential-delay.js
+++ src/strategies/delay/exponential-delay.js
@@ -4,13 +4,13 @@ const Delay = require('@emartech/delay-js');
 
 class ExponentialDelay {
   constructor(tries, multiplier = 1, exponentialBase = 2) {
-    this.multiplier = multiplier;
+    this._multiplier = multiplier;
     [...this._delayInSeconds] = this._exponential(tries - 1, exponentialBase);
   }
 
   delay(attempts) {
     const delayInSeconds = this._delayInSeconds[attempts - 1];
-    const delayInMilliSeconds = delayInSeconds * 1000 * this.multiplier;
+    const delayInMilliSeconds = delayInSeconds * 1000 * this._multiplier;
     return Delay.wait(delayInMilliSeconds, delayInSeconds);
   }
 
